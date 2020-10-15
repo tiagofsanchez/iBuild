c5ceed0f114d0d1eddcded78e968965339f44049
Tiago Sanchez <tiagofsanchez@gmail.com>
2020-10-15 13:46:17 +0800

    fixed the svg issue

--- frontend/src/components/ui/heroHeader.js
+++ frontend/src/components/ui/heroHeader.js
@@ -1,20 +1,16 @@
 /** @jsx jsx */
 import { jsx } from "theme-ui";
 import styled from "@emotion/styled";
-import Hero from "../../svgs/heroHeader.svg";
 
 const Container = styled.div`
   position: absolute;
   z-index: -10;
   width: 100%;
+  height: 420px;
 `;
 
 const HeroHeader = () => {
-  return (
-    <Container>
-      <Hero />
-    </Container>
-  );
+  return <Container sx={{ backgroundColor: `naturalDark` }} />;
 };
 
 export default HeroHeader;
